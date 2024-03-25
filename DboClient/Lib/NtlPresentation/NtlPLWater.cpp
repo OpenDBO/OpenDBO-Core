eefeb5c629035b71a4d24ad30f338003a2f1f6fa
GrenderG <grenderg@gmail.com>
2024-03-25 16:41:31 +0100

    Optimize https://github.com/OpenDBO/OpenDBO-Core/commit/0340bd9a937d6454ac53764f2f622a3cbfea1207 a bit

--- DboClient/Lib/NtlPresentation/NtlPLWater.cpp
+++ DboClient/Lib/NtlPresentation/NtlPLWater.cpp
@@ -406,33 +406,28 @@ RwBool CNtlPLWater::OnRender(CNtlWorldSector* pNtlWorldSector, RxD3D9InstanceDat
 
 			WATER_ITER	iter;
 			sWATER*		pWater;
-			iter = m_mapWater.find(WATER_MAP::key_type(pSectorWaterAttr->_IdxSequence));
+
+			// Hackfix?
+			// 
+			// For some reason pSectorWaterAttr->_IdxSequence sometimes is like normal Idx + 1000.
+			// Probably for some feature added between 1.1 and 1.4 map versions.
+			// For example river water has 1001 as Idx instead of 1 so if we don't do this it won't
+			// be loaded.
+			// 
+			// TODO: Check if water is correct everywhere.
+			// TODO: Find out why Idx is + 1000, maybe we need to render some special effects?
+			//iter = m_mapWater.find(WATER_MAP::key_type(pSectorWaterAttr->_IdxSequence));
+			iter = m_mapWater.find(WATER_MAP::key_type(pSectorWaterAttr->_IdxSequence % 1000));
 			if(iter != m_mapWater.end())
 			{
 				pWater = iter->second;
 			}
 			else
 			{
-				//NTL_RPROFILE(FALSE)
-
-				// Hackfix?
-				// For some reason pSectorWaterAttr->_IdxSequence sometimes is like normal Idx + 1000,
-				// Probably for some feature added between 1.1 and 1.4 map version.
-				// TODO: Check if water is correct everywhere
-				// TODO: Find out why Idx is +1000, maybe we need to render some special effects?
-				iter = m_mapWater.find(WATER_MAP::key_type(pSectorWaterAttr->_IdxSequence % 1000));
-
-				if (iter != m_mapWater.end())
-				{
-					pWater = iter->second;
-				}
-				else
-				{
-					char buffer[1024];
-					sprintf_s(buffer, sizeof buffer, "Invalid pSectorWaterAttr->_IdxSequence: %u\n", pSectorWaterAttr->_IdxSequence);
-					NtlLogFilePrint(buffer);
-					NTL_RPROFILE(FALSE)
-				}
+				char buffer[1024];
+				sprintf_s(buffer, sizeof buffer, "Invalid pSectorWaterAttr->_IdxSequence: %u\n", pSectorWaterAttr->_IdxSequence);
+				NtlLogFilePrint(buffer);
+				NTL_RPROFILE(FALSE)
 			}
 
 			RwRenderStateSet(rwRENDERSTATESRCBLEND, (void*)pNtlWorldSector->m_pWater->_RenderstateSrc);
