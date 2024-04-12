e57400380fcc242507e250c6405424f9c3a89c31
GrenderG <grenderg@gmail.com>
2024-04-12 23:45:08 +0200

    Update NetPy reward to match latest known TW official value.

--- DboShared/NtlShared2/NtlNetP.h
+++ DboShared/NtlShared2/NtlNetP.h
@@ -3,6 +3,10 @@
 #define MAX_NETP_POINT ( 0xfffffffeui32 )
 
 const DWORD	NETPY_POINTS_TIME_DURATION = 900;
-const DWORD	NETPY_POINTS_TIME_REWARD = 10;
 const DWORD NETPY_MAX_SHOPPOINTS = 50000;
-const DWORD NETPY_POINTS_REWARD = 7;
+// Latest known TW info mentions 4 points every 15 minutes (was 1 every 15 minutes at first).
+// Everyone says 14 but that's because UI shows 14 due to seconds being simply trimmed on 14:XX.
+// http://www.dbocom.com/forum/showthread.php?p=336784#post336784
+// TODO: Also they should be account bound, not character bound.
+const DWORD	NETPY_POINTS_TIME_REWARD = 15;
+const DWORD NETPY_POINTS_REWARD = 4;
