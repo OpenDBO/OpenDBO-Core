7b9bd83c0436ffee700065f73ca917efa52a1590
GrenderG <grenderg@gmail.com>
2025-06-25 17:34:54 +0200

    Remove hardcoded strings.

--- DboClient/Client/Gui/DisplayStringManager.cpp
+++ DboClient/Client/Gui/DisplayStringManager.cpp
@@ -50,43 +50,6 @@ const WCHAR * CDisplayStringManager::GetString(std::string key, RwUInt32 eType)
 			return it->second.c_str();
 	}
 
-	// Provide default values for specific keys (hardcoded)
-	if (key == "DST_OPTION_FILTER_RARITY") return L"Rarity";
-	if (key == "DST_OPTION_TOOLTIP_FILTER_NORMAL") return L"Normal items";
-	if (key == "DST_OPTION_FILTER_RARITY_NORMAL") return L"Normal";
-	if (key == "DST_OPTION_TOOLTIP_FILTER_SUPERIOR") return L"Superior items";
-	if (key == "DST_OPTION_FILTER_RARITY_SUPERIOR") return L"Superior";
-	if (key == "DST_OPTION_TOOLTIP_FILTER_EXCELLENT") return L"Excellent items";
-	if (key == "DST_OPTION_FILTER_RARITY_EXCELLENT") return L"Excellent";
-	if (key == "DST_OPTION_TOOLTIP_FILTER_RARE") return L"Rare items";
-	if (key == "DST_OPTION_FILTER_RARITY_RARE") return L"Rare";
-	if (key == "DST_OPTION_TOOLTIP_FILTER_LEGENDARY") return L"Legendary items";
-	if (key == "DST_OPTION_FILTER_RARITY_LEGENDARY") return L"Legendary";
-	if (key == "DST_OPTION_FILTER_STONES") return L"Stones";
-	if (key == "DST_OPTION_TAB_EXTRA") return L"Extra";
-	if (key == "DST_OPTION_TOOLTIP_TAB_EXTRA") return L"Extra stuff added";
-	if (key == "DST_OPTION_TAB_EXTRA_FILTER") return L"Drop Filter";
-	if (key == "DST_OPTION_TOOLTIP_TAB_EXTRA_FILTER") return L"Option for Drop Filter";
-	if (key == "DST_OPTION_FILTER_STONE_BLUE") return L"Blue Stone";
-	if (key == "DST_OPTION_TOOLTIP_FILTER_STONE_BLUE") return L"Armor Normal Stone";
-	if (key == "DST_OPTION_FILTER_STONE_RED") return L"Red Stone";
-	if (key == "DST_OPTION_TOOLTIP_FILTER_STONE_RED") return L"Weapon Normal Stone";
-	if (key == "DST_OPTION_FILTER_STONE_GREEN") return L"Green Stone";
-	if (key == "DST_OPTION_TOOLTIP_FILTER_STONE_GREEN") return L"Armor Special Stone";
-	if (key == "DST_OPTION_FILTER_STONE_PURPLE") return L"Purple Stone";
-	if (key == "DST_OPTION_TOOLTIP_FILTER_STONE_PURPLE") return L"Weapon Special Stone";
-	if (key == "DST_OPTION_FILTER_STONE_WHITE") return L"White Stone";
-	if (key == "DST_OPTION_TOOLTIP_FILTER_STONE_WHITE") return L"Stone that protects upgrade";
-	if (key == "DST_OPTION_FILTER_STONE_ADOWN") return L"A. Grey Stone";
-	if (key == "DST_OPTION_TOOLTIP_FILTER_STONE_ADOWN") return L"Armor Downgrade Stone";
-	if (key == "DST_OPTION_FILTER_STONE_WDOWN") return L"W. Grey Stone";
-	if (key == "DST_OPTION_TOOLTIP_FILTER_STONE_WDOWN") return L"Weapon Downgrade Stone";
-	if (key == "DST_OPTION_FILTER_MYSTERYITEM") return L"Mystery Item";
-	if (key == "DST_OPTION_TOOLTIP_FILTER_MYSTERYITEM") return L"The capsules that need to be identified";
-	if (key == "DST_OPTION_FILTER_MISC") return L"Misc Items";
-	if (key == "DST_OPTION_FILTER_CONSUMABLE") return L"Consumables";
-	if (key == "DST_OPTION_TOOLTIP_FILTER_CONSUMABLE") return L"LP, EP restoration and treatment pills (poison, abdominal, etc...)";
-
 	DBO_WARNING_MESSAGE("Missing string with key: " << key);
 	static WCHAR awcBuffer[] = L"String not found";
 
