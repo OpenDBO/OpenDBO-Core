e4ecec131c125dddf5302083f73b0d5fbbc50b25
unknown <ricardo.goncalves252@gmail.com>
2026-08-03 21:42:10 +0100

    chore: remove some comments

--- DboClient/Client/BroadCastSystem.cpp
+++ DboClient/Client/BroadCastSystem.cpp
@@ -38,9 +38,6 @@ RwBool CBroadCastSystem::Create()
 {
 	NTL_FUNCTION("CBroadCastSystem::Create");
 
-	/*if (!CNtlPLGui::Create("", "gui\\BroadCastSystem.srf", "gui\\BroadCastSystem.frm"))
-		NTL_RETURN(FALSE);
-		*/
 	if (!CNtlPLGui::Create("", "gui\\Commercial.srf", "gui\\Commercial.frm"))
 		NTL_RETURN(FALSE);
 
@@ -48,27 +45,20 @@ RwBool CBroadCastSystem::Create()
 	CNtlPLGui::CreateComponents(CNtlPLGuiManager::GetInstance()->GetGuiManager());
 
 	m_pThis = (gui::CDialog*)GetComponent("dlgMain");
-	m_pThis->SetPriority(dDIALOGPRIORITY_MSGBOX); // 设置成优先级
+	m_pThis->SetPriority(dDIALOGPRIORITY_MSGBOX);
 	m_pThis->SetClippingMode(true);
 	m_stBackGround = (gui::CStaticBox*)GetComponent("stBackGround");
 	m_stBackGround->SetClippingMode(true);
-	m_stString = (gui::CStaticBox*)GetComponent("stString"); // 第一个文字
+	m_stString = (gui::CStaticBox*)GetComponent("stString");
 	m_stString->SetClippingMode(true);
-	m_stString->SetText("");// 初始化一下不然会崩溃
-	m_stSecoundString = (gui::CStaticBox*)GetComponent("stSecoundString");	// 第二个文字
+	m_stString->SetText("");
+	m_stSecoundString = (gui::CStaticBox*)GetComponent("stSecoundString");
 	m_stSecoundString->SetClippingMode(true);
 	m_stSecoundString->SetText("");
 
 	m_slotMouseEnter = m_pThis->SigMouseEnter().Connect(this, &CBroadCastSystem::OnMouseEnter);
 	m_slotMouseLeave = m_pThis->SigMouseLeave().Connect(this, &CBroadCastSystem::OnMouseLeave);
 
-	/*m_pnlBackGround = (gui::CPanel*)GetComponent("backPanelSystem");
-	m_paStbInfoBox = (gui::CStaticBox*)GetComponent("StbInfoBox");*/
-
-	//m_pThis->GetClientRect();
-	// 等调用show的时候在设置定位
-	
-	// 设置强化框的位置
 	LocateComponent(GetDboGlobal()->GetScreenWidth(), GetDboGlobal()->GetScreenHeight());
 
 	GetNtlGuiManager()->AddUpdateFunc(this);
@@ -76,7 +66,7 @@ RwBool CBroadCastSystem::Create()
 	Show(false);
 
 	LinkMsg(g_EventBroadCastNfy, 0);
-	LinkMsg(g_EventResize, 0); // 调整窗口时 重定位
+	LinkMsg(g_EventResize, 0);
 	InitStringBoxFirst();
 	InitStringBoxSecound();
 	NTL_RETURN(TRUE);
@@ -123,7 +113,6 @@ VOID CBroadCastSystem::HandleEvents(RWS::CMsg& msg)
 	{
 		WCHAR Text[500];
 
-		// 强化的聊天框
 		if (pNotify->MsgType == DBO_BROADCASTING_MSG_TYPE_ITEMUPGRADE) 
 		{
 			swprintf_s(Text, 500, GetDisplayStringManager()->GetString("DST_COMMERCIAL_ITEMUPGRADE"), pNotify->pData.sItemUpgrade.wszName, Logic_GetItemName(pNotify->pData.sItemUpgrade.tblidx), pNotify->pData.sItemUpgrade.byGrade);
@@ -143,8 +132,6 @@ VOID CBroadCastSystem::HandleEvents(RWS::CMsg& msg)
 		}
 		else if (pNotify->MsgType == DBO_BROADCASTING_MSG_TYPE_BUDOKAI) 
 		{
-
-			// 设置获胜者的排行文字
 			std::wstring winner;
 			if (pNotify->pData.sBudokaiRecord.byRank == 0) {
 				winner = GetDisplayStringManager()->GetString("DST_BUDOKAI_PC_STATE_FINAL_WINNER");
@@ -156,11 +143,10 @@ VOID CBroadCastSystem::HandleEvents(RWS::CMsg& msg)
 				winner = GetDisplayStringManager()->GetString("DST_BUDOKAI_PC_STATE_PRIZE_WINNER");
 			}
 
-			if (pNotify->pData.sBudokaiRecord.byMatchType == BUDOKAI_MATCH_TYPE_INDIVIDIAUL) // 个人武道会
+			if (pNotify->pData.sBudokaiRecord.byMatchType == BUDOKAI_MATCH_TYPE_INDIVIDIAUL)
 			{
-				if (pNotify->pData.sBudokaiRecord.byBudokaiType == BUDOKAI_TYPE_JUNIOR) // 判断是成人还是少年
+				if (pNotify->pData.sBudokaiRecord.byBudokaiType == BUDOKAI_TYPE_JUNIOR)
 				{
-					// 设置文字
 					swprintf_s(Text, 500, GetDisplayStringManager()->GetString("DST_COMMERCIAL_BUDOKAI"),
 						pNotify->pData.sBudokaiRecord.wSeason,
 						GetDisplayStringManager()->GetString("DST_BUDOKAI_TYPE_CHILD_TITLE"),
@@ -178,7 +164,6 @@ VOID CBroadCastSystem::HandleEvents(RWS::CMsg& msg)
 				}
 				else 
 				{
-					// 设置文字
 					swprintf_s(Text, 500, GetDisplayStringManager()->GetString("DST_COMMERCIAL_BUDOKAI"),
 						pNotify->pData.sBudokaiRecord.wSeason,
 						GetDisplayStringManager()->GetString("DST_BUDOKAI_TYPE_ADULT_TITLE"),
@@ -195,11 +180,10 @@ VOID CBroadCastSystem::HandleEvents(RWS::CMsg& msg)
 					m_iTextValues++;
 				}
 			}
-			else {// 否则就是团队武道会
+			else {
 				
 				std::wstring CharName;
 
-				// 循环增加玩家名称
 				for (int i = 0; i < pNotify->pData.sBudokaiRecord.byMemberCount; i++) {
 					CharName += pNotify->pData.sBudokaiRecord.awszMember[i];
 					if (i != pNotify->pData.sBudokaiRecord.byMemberCount - 1) {
@@ -207,9 +191,8 @@ VOID CBroadCastSystem::HandleEvents(RWS::CMsg& msg)
 					}
 				}
 
-				if (pNotify->pData.sBudokaiRecord.byBudokaiType == BUDOKAI_TYPE_JUNIOR) // 判断是成人还是少年
+				if (pNotify->pData.sBudokaiRecord.byBudokaiType == BUDOKAI_TYPE_JUNIOR)
 				{
-					// 设置文字
 					swprintf_s(Text, 500, GetDisplayStringManager()->GetString("DST_COMMERCIAL_BUDOKAI"),
 						pNotify->pData.sBudokaiRecord.wSeason,
 						GetDisplayStringManager()->GetString("DST_BUDOKAI_TYPE_CHILD_TITLE"),
@@ -227,7 +210,6 @@ VOID CBroadCastSystem::HandleEvents(RWS::CMsg& msg)
 				}
 				else 
 				{
-					// 设置文字
 					swprintf_s(Text, 500, GetDisplayStringManager()->GetString("DST_COMMERCIAL_BUDOKAI"),
 						pNotify->pData.sBudokaiRecord.wSeason,
 						GetDisplayStringManager()->GetString("DST_BUDOKAI_TYPE_ADULT_TITLE"),
@@ -249,7 +231,7 @@ VOID CBroadCastSystem::HandleEvents(RWS::CMsg& msg)
 		}
 		else if (pNotify->MsgType == DBO_BROADCASTING_MSG_TYPE_WAGUWAGU_1ST) 
 		{
-			// 设置文字
+			/* TODO: Uncomment when wagu system is done
 			swprintf_s(Text, 500, GetDisplayStringManager()->GetString("DST_WAGUWAGU_BROADINFO"), pNotify->pData.sWaguWagu1st.wszName, Logic_GetItemName(pNotify->pData.sWaguWagu1st.itemTblidx));
 
 			if (m_iTextValues % 2 == 0)
@@ -259,18 +241,19 @@ VOID CBroadCastSystem::HandleEvents(RWS::CMsg& msg)
 
 			Show(true);
 			m_iTextValues++;
+			*/
 		}
 		else if (pNotify->MsgType == DBO_BROADCASTING_MSG_TYPE_CCBD) 
 		{
 			std::wstring CharName;
-			// 循环增加玩家名称
+
 			for (int i = 0; i < pNotify->pData.sCCBD.byMemberCount;i++) {
 				CharName += pNotify->pData.sCCBD.awszMember[i];
 				if (i != pNotify->pData.sCCBD.byMemberCount - 1) {
 					CharName += L", ";
 				}
 			}
-			// 设置文字
+
 			swprintf_s(Text, 500, GetDisplayStringManager()->GetString("DST_BROADCAST_CCBD_CLEAR"), CharName.c_str(), pNotify->pData.sCCBD.byStage);
 			if (m_iTextValues % 2 == 0)
 				m_sTextTwo.push_back(Text);
@@ -293,13 +276,12 @@ void CBroadCastSystem::Update(RwReal fElapsed)
 	{
 	if (!m_sTextOne.empty())
 	{
-		if(m_bIsTwoTextOver || m_stString->GetScreenRect().right < m_stSecoundString->GetScreenRect().left)// 这里不至于结束后无法播放
+		if(m_bIsTwoTextOver || m_stString->GetScreenRect().right < m_stSecoundString->GetScreenRect().left)
 		{
-			// 第一个文字
 			m_fBoxPresentPosX -= (fElapsed * (RwReal)g_nTestVal);
 			m_stString->SetPosition(m_fBoxPresentPosX, 19);
 			if (m_stString->GetText().empty())
-				m_stString->SetText(m_sTextOne.front().c_str()); // 获取第一个元素并给第一个文本框赋值
+				m_stString->SetText(m_sTextOne.front().c_str());
 
 			if (m_stString->GetScreenRect().right < m_stBackGround->GetScreenRect().right)
 				m_bIsOneTextOver = true;
@@ -308,15 +290,15 @@ void CBroadCastSystem::Update(RwReal fElapsed)
 
 			if (m_stString->GetScreenRect().right < m_stBackGround->GetScreenRect().left)
 			{
-				InitStringBoxFirst();//初始化窗口位置
-				m_sTextOne.erase(m_sTextOne.begin()); // 弹出文本
+				InitStringBoxFirst();
+				m_sTextOne.erase(m_sTextOne.begin());
 			}
 		}
 	}
 			
 	if (!m_sTextTwo.empty()) 
 	{
-		if(m_bIsOneTextOver || m_stSecoundString->GetScreenRect().right < m_stString->GetScreenRect().left)// 这里不至于结束后无法播放
+		if(m_bIsOneTextOver || m_stSecoundString->GetScreenRect().right < m_stString->GetScreenRect().left)
 		{
 			m_fBoxPresentPosXTwo -= (fElapsed * (RwReal)g_nTestVal);
 			m_stSecoundString->SetPosition(m_fBoxPresentPosXTwo, 19);
@@ -337,7 +319,7 @@ void CBroadCastSystem::Update(RwReal fElapsed)
 	}
 	}
 
-	if (m_sTextOne.empty() && m_sTextTwo.empty()) // 如果容器不为空则滚动文字
+	if (m_sTextOne.empty() && m_sTextTwo.empty())
 	{
 		if (m_pThis->IsVisible())
 		{
@@ -358,8 +340,8 @@ void CBroadCastSystem::InitStringBoxFirst()
 	CRectangle rtScreen = m_stBackGround->GetScreenRect();
 	m_bIsOneTextOver = true;
 	m_fBoxPresentPosX = (RwReal)rtScreen.GetWidth() + 15;
-	m_stString->Clear(); // 初始化文字
-	m_stString->SetPosition(m_fBoxPresentPosX, 19); // 初始化定位
+	m_stString->Clear();
+	m_stString->SetPosition(m_fBoxPresentPosX, 19);
 }
 
 void CBroadCastSystem::InitStringBoxSecound()
